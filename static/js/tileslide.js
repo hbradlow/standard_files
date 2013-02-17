036727c6f3fc5aaa2c0fb14e2a02367e53a5897b
Henry Bradlow <hbradlow@berkeley.edu>
2013-02-16 21:51:28 -0800

    Added more django basic stuff

---
+++ static/js/tileslide.js
@@ -0,0 +1,35 @@
+(function($){
+    $.fn.tileslide = function(options){
+        if(!options) options = {};
+        var default_args = {
+            "slide_time":   300,
+            "slide_height": 50,
+            "on":function(){},
+            "off":function(){},
+        }
+        for(var arg in default_args){
+            if(typeof options[arg] == "undefined"){
+                options[arg] = default_args[arg];
+            }
+        }
+
+        var img_height = parseInt($(this).find(".tile_img").css("height"));
+
+        var text_height = parseInt($(this).find(".tile_text").css("height"));
+
+        $(this).hover(function(){
+            options["on"]();
+            $(this).css("z-index","100");
+            $(this).find(".tile_img").stop().animate({"top":"-"+options["slide_height"]+"px"},options["slide_time"]);
+            $(this).find(".tile_text").stop().animate({"top":String(img_height-options["slide_height"])+"px"},options["slide_time"]);
+        },function(){
+            options["off"]();
+            var tile = this;
+            $(tile).css("z-index","99");
+            $(this).find(".tile_img").stop().animate({"top":"0px"},options["slide_time"],function(){
+                $(tile).css("z-index","1");
+            });
+            $(this).find(".tile_text").stop().animate({"top":String(img_height-text_height)+"px"},options["slide_time"]);
+        });
+    };
+})(jQuery);
